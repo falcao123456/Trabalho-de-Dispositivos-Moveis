2bdc2fa1f0226dc5b1e0ca22be1f2ff5503fd9be
tacio correia <taciocorreia.tc@gmail.com>
2020-10-04 14:28:45 -0300

    adicionnado menu em todas as telas junto com os dropdown e mudanças no visual de menus e algumas funções

---
+++ views/dropdown/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>
+	<div>
+		<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Botão dropdown
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Alguma ação</a>
+    <a class="dropdown-item" href="#">Outra ação</a>
+    <a class="dropdown-item" href="#">Alguma coisa aqui</a>
+  </div>
+</div>
+	</div>
+</body>
+</html>
